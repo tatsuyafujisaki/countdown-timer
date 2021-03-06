9ebabe758f44cf58cca8e9b5efb49288492a8998
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-03-06 11:53:50 +0900

    Do spotlessApply

--- app/src/main/java/com/example/androiddevchallenge/CountdownTimerScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/CountdownTimerScreen.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import android.os.CountDownTimer
@@ -224,8 +239,8 @@ fun CountDownTimerScreen(modifier: Modifier = Modifier) {
                             TimerState.Running
                         }
                     }
-                })
-            {
+                }
+            ) {
                 Crossfade(timerState) {
                     when (it) {
                         TimerState.Running -> {
@@ -247,11 +262,13 @@ fun CountDownTimerScreen(modifier: Modifier = Modifier) {
                     }
                 }
             }
-            Button(onClick = {
-                countDownTimer.cancel()
-                remainingTotalSeconds = totalSeconds
-                timerState = TimerState.Stopped
-            }) {
+            Button(
+                onClick = {
+                    countDownTimer.cancel()
+                    remainingTotalSeconds = totalSeconds
+                    timerState = TimerState.Stopped
+                }
+            ) {
                 Text("Reset")
             }
         }
@@ -271,4 +288,3 @@ fun CountDownTimerScreen(modifier: Modifier = Modifier) {
         }
     }
 }
-
